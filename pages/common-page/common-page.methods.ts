47d4609ce64c5b1aa82e1ed19512dfcf68f2939e
Damian991982 <ing.damian.almada@gmail.com>
2026-02-02 18:57:50 -0300

    fix page variable type

---
+++ pages/common-page/common-page.methods.ts
@@ -0,0 +1,10 @@
+import { Page } from "@playwright/test";
+export class CommonPageMethods {
+  private page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+  async navigateToTheAppApplication() {
+    await this.page.goto("https://www.saucedemo.com/");
+  }
+}
