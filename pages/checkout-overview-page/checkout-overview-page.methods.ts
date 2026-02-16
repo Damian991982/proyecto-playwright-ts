ce1f10deef45f09f9b844efd772d7fc83d520b34
Damian991982 <ing.damian.almada@gmail.com>
2026-02-16 01:45:42 -0300

    create class checkoutOverviewPageMethods

--- pages/checkout-overview-page/checkout-overview-page.methods.ts
+++ pages/checkout-overview-page/checkout-overview-page.methods.ts
@@ -1,1 +1,18 @@
-export class CheckoutOverviewPageMethods {}
+import { Page } from "@playwright/test";
+import { CheckoutOverviewPageElements } from "./checkout-overview-page.elements";
+export class CheckoutOverviewPageMethods {
+  private page: Page;
+  private checkoutOverviewPageElements: CheckoutOverviewPageElements;
+  constructor(page: Page) {
+    this.page = page;
+    this.checkoutOverviewPageElements = new CheckoutOverviewPageElements(page);
+  }
+
+  async clickOnCancelButton() {
+    await this.checkoutOverviewPageElements.buttons.cancel.click();
+  }
+
+  async clickOnFinishButton() {
+    await this.checkoutOverviewPageElements.buttons.finish.click();
+  }
+}
