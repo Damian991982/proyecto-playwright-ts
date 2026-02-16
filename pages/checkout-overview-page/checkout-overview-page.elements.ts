8d5ac0307eac87da354e407c7e14f42b068b6263
Damian991982 <ing.damian.almada@gmail.com>
2026-02-16 00:50:15 -0300

    create class checkoutPageElements

---
+++ pages/checkout-overview-page/checkout-overview-page.elements.ts
@@ -0,0 +1,14 @@
+import { Page } from "@playwright/test";
+export class CheckoutOverviewPageElements {
+  private page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  get buttons() {
+    return {
+      cancel: this.page.locator("#cancel"),
+      finish: this.page.locator(".cart_button"),
+    };
+  }
+}
